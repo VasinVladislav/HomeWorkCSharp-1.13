b606b617a767f6ae5ad12810b617bdaa04646b93
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 13:28:30 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.13.Составить программу вывода на экран следующей информации:
+            //2 кг
+            //13 17
+            int a = 2; string b = "кг";
+            int c = 13; int d = 17;
+            Console.WriteLine($"{a} {b}\n{c} {d}");
+        }
+    }
+}
---
+++ HomeWorkCSharp-1.13/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HomeWorkCSharp-1.13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWorkCSharp-1.13")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a436aefd-4e81-49ca-9a01-ff7d1dd2ab27")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
